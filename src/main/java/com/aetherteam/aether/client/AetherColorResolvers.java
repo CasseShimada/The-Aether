7b1344eb1bac73dff173bd842691ec3f4911bfe5
CasseShimada <c382304209@gmail.com>
2026-03-06 17:46:02 +0800

    Fix client startup color resolver crash and drop incompatible client accessor mixins.

--- src/main/java/com/aetherteam/aether/client/AetherColorResolvers.java
+++ src/main/java/com/aetherteam/aether/client/AetherColorResolvers.java
@@ -2,45 +2,35 @@ package com.aetherteam.aether.client;
 
 import com.aetherteam.aether.AetherTags;
 import com.aetherteam.aether.block.AetherBlocks;
-import com.aetherteam.aether.mixin.mixins.client.accessor.BlockColorsAccessor;
 import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.color.block.BlockColor;
+import net.minecraft.client.renderer.BiomeColors;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.DoublePlantBlock;
 import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
-
-import java.util.HashMap;
-import java.util.Map;
+import net.minecraft.world.level.GrassColor;
 
 public class AetherColorResolvers {
     private static final int AETHER_GRASS_COLOR = 0xB1FFCB;
     private static final int ENCHANTED_GRASS_COLOR = 0xFCEA64;
 
     public static void registerBlockColor() {
-        Map<Block, BlockColor> map = new HashMap<>();
-        Map<Block, BlockColor> blockColors = ((BlockColorsAccessor) Minecraft.getInstance().getBlockColors()).aether$getBlockColors();
-        map.put(Blocks.SHORT_GRASS, blockColors.get(Blocks.SHORT_GRASS));
-        map.put(Blocks.FERN, blockColors.get(Blocks.FERN));
-        map.put(Blocks.TALL_GRASS, blockColors.get(Blocks.TALL_GRASS));
-        map.put(Blocks.LARGE_FERN, blockColors.get(Blocks.LARGE_FERN));
-
-        for (Map.Entry<Block, BlockColor> entry : map.entrySet()) { // Recolors tintable plants when placed on Aether Grass and Enchanted Grass.
-            ColorProviderRegistry.BLOCK.register(((state, level, pos, tintIndex) -> {
-                if (level != null && pos != null) {
-                    BlockPos newPos = state.hasProperty(DoublePlantBlock.HALF) ? (state.getValue(DoublePlantBlock.HALF) == DoubleBlockHalf.UPPER ? pos.below() : pos) : pos;
-                    BlockPos baseBlock = newPos.below();
-                    if (level.getBlockState(baseBlock).is(AetherTags.Blocks.ENCHANTED_GRASS)) {
-                        return ENCHANTED_GRASS_COLOR;
-                    } else if (level.getBlockState(baseBlock).is(AetherBlocks.AETHER_GRASS_BLOCK.get())) {
-                        return AETHER_GRASS_COLOR;
-                    }
+        BlockColor tintedPlantColor = (state, level, pos, tintIndex) -> {
+            if (level != null && pos != null) {
+                BlockPos newPos = state.hasProperty(DoublePlantBlock.HALF) ? (state.getValue(DoublePlantBlock.HALF) == DoubleBlockHalf.UPPER ? pos.below() : pos) : pos;
+                BlockPos baseBlock = newPos.below();
+                if (level.getBlockState(baseBlock).is(AetherTags.Blocks.ENCHANTED_GRASS)) {
+                    return ENCHANTED_GRASS_COLOR;
+                } else if (level.getBlockState(baseBlock).is(AetherBlocks.AETHER_GRASS_BLOCK.get())) {
+                    return AETHER_GRASS_COLOR;
                 }
-                return entry.getValue().getColor(state, level, pos, tintIndex);
-            }), entry.getKey());
-        }
+                return BiomeColors.getAverageGrassColor(level, newPos);
+            }
+            return GrassColor.getDefaultColor();
+        };
+        ColorProviderRegistry.BLOCK.register(tintedPlantColor, Blocks.SHORT_GRASS, Blocks.FERN, Blocks.TALL_GRASS, Blocks.LARGE_FERN);
     }
 
     public static void registerItemColor() {
