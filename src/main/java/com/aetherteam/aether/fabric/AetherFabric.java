e352d49e80e56aaad3e21e57cb8d340c3bfa8561
CasseShimada <c382304209@gmail.com>
2026-03-06 17:59:41 +0800

    Register Fabric portal activation callback and restore water bucket activation tags.

--- src/main/java/com/aetherteam/aether/fabric/AetherFabric.java
+++ src/main/java/com/aetherteam/aether/fabric/AetherFabric.java
@@ -1,16 +1,20 @@
 package com.aetherteam.aether.fabric;
 
 import com.aetherteam.aether.Aether;
+import com.aetherteam.aether.event.hooks.DimensionHooks;
 import com.aetherteam.aether.command.AetherCommands;
 import com.aetherteam.aether.network.AetherNetworking;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.world.InteractionResult;
 
 public class AetherFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         Aether.init();
         AetherNetworking.registerCommon();
+        UseBlockCallback.EVENT.register((player, level, hand, hitResult) -> DimensionHooks.createPortal(player, level, hitResult.getBlockPos(), hitResult.getDirection(), player.getItemInHand(hand), hand) ? InteractionResult.SUCCESS : InteractionResult.PASS);
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> AetherCommands.registerCommands(dispatcher));
     }
 }
