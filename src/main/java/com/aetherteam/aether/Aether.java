60ddd2501ab1bf48f3c5b355d765bd6d1e68c750
CasseShimada <c382304209@gmail.com>
2026-03-15 23:37:29 +0800

    Register Aether portal POI states on Fabric

--- src/main/java/com/aetherteam/aether/Aether.java
+++ src/main/java/com/aetherteam/aether/Aether.java
@@ -69,6 +69,7 @@ public final class Aether {
         Reflection.initialize(AetherDataAttachments.class);
         AetherBlocks.registerWoodTypes();
         registerContent();
+        AetherPoi.registerBlockStateMappings();
         AetherEntityTypes.registerEntityAttributes();
         AetherEntityTypes.registerSpawnPlacements();
 
--- src/main/java/com/aetherteam/aether/world/AetherPoi.java
+++ src/main/java/com/aetherteam/aether/world/AetherPoi.java
@@ -2,7 +2,9 @@ package com.aetherteam.aether.world;
 
 import com.aetherteam.aether.Aether;
 import com.aetherteam.aether.block.AetherBlocks;
+import com.aetherteam.aether.mixin.mixins.common.accessor.PoiTypesAccessor;
 import com.google.common.collect.ImmutableSet;
+import net.minecraft.core.Holder;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.entity.ai.village.poi.PoiType;
 import net.minecraft.world.level.block.Block;
@@ -16,6 +18,11 @@ public class AetherPoi {
     public static final DeferredRegister<PoiType> POI = DeferredRegister.create(BuiltInRegistries.POINT_OF_INTEREST_TYPE, Aether.MODID);
 
     public static final DeferredHolder<PoiType, PoiType> AETHER_PORTAL = POI.register("aether_portal", () -> new PoiType(getBlockStates(AetherBlocks.AETHER_PORTAL.get()), 0, 1));
+
+    public static void registerBlockStateMappings() {
+        Holder<PoiType> holder = BuiltInRegistries.POINT_OF_INTEREST_TYPE.wrapAsHolder(AETHER_PORTAL.get());
+        PoiTypesAccessor.aether$registerBlockStates(holder, getBlockStates(AetherBlocks.AETHER_PORTAL.get()));
+    }
 
     private static Set<BlockState> getBlockStates(Block block) {
         return ImmutableSet.copyOf(block.getStateDefinition().getPossibleStates());
