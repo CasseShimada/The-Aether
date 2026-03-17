254ed8e0da3f19c17da78b1591b0e0efdafc6e5d
CasseShimada <c382304209@gmail.com>
2026-03-18 05:57:08 +0800

    Restore cape model-part visibility when cape accessory is equipped

---
+++ src/main/java/com/aetherteam/aether/mixin/mixins/common/AvatarMixin.java
@@ -0,0 +1,20 @@
+package com.aetherteam.aether.mixin.mixins.common;
+
+import com.aetherteam.aether.mixin.AetherMixinHooks;
+import net.minecraft.world.entity.Avatar;
+import net.minecraft.world.entity.player.PlayerModelPart;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Avatar.class)
+public class AvatarMixin {
+    @Inject(method = "isModelPartShown(Lnet/minecraft/world/entity/player/PlayerModelPart;)Z", at = @At("HEAD"), cancellable = true)
+    private void aether$showCapeModelPartWhenAccessoryEquipped(PlayerModelPart part, CallbackInfoReturnable<Boolean> cir) {
+        Avatar avatar = (Avatar) (Object) this;
+        if (!AetherMixinHooks.isCapeVisible(avatar).isEmpty()) {
+            cir.setReturnValue(true);
+        }
+    }
+}
