d7e7f76d4a4c57a2a1d8e7d8439d36d018fb24b3
CasseShimada <c382304209@gmail.com>
2026-04-05 21:18:41 +0800

    Restore vanilla accessory elytra takeoff behavior

--- src/main/java/com/aetherteam/aether/mixin/mixins/common/PlayerMixin.java
+++ src/main/java/com/aetherteam/aether/mixin/mixins/common/PlayerMixin.java
@@ -1,7 +1,6 @@
 package com.aetherteam.aether.mixin.mixins.common;
 
 import com.aetherteam.aether.entity.passive.MountableAnimal;
-import com.aetherteam.aether.accessories.compat.AccessoryEffectBridge;
 import com.aetherteam.aether.event.hooks.AbilityHooks;
 import com.aetherteam.aether.event.hooks.CapabilityHooks;
 import com.aetherteam.aether.event.hooks.DimensionHooks;
@@ -13,19 +12,15 @@ import com.llamalad7.mixinextras.sugar.ref.LocalBooleanRef;
 import net.minecraft.util.RandomSource;
 import net.minecraft.util.Util;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 
@@ -33,12 +28,6 @@ import java.util.List;
 public abstract class PlayerMixin {
     @Shadow
     protected abstract boolean wantsToStopRiding();
-
-    @Shadow
-    public abstract void startFallFlying();
-
-    @Shadow
-    protected abstract boolean canGlide();
 
     /**
      * Damages gloves only once during a sweeping attack, instead of once for every damaged entity in the attack.
@@ -116,22 +105,5 @@ public abstract class PlayerMixin {
         speed = AbilityHooks.AccessoryHooks.handleZanitePendantAbility(player, speed);
         speed = AbilityHooks.ToolHooks.handleZaniteToolAbility(stack, speed);
         return AbilityHooks.ToolHooks.reduceToolEffectiveness(player, state, stack, speed);
-    }
-
-    @Inject(method = "tryToStartFallFlying()Z", at = @At("HEAD"), cancellable = true)
-    private void aether$startAccessoryElytraFlight(CallbackInfoReturnable<Boolean> cir) {
-        Player player = (Player) (Object) this;
-        if (player.getItemBySlot(EquipmentSlot.CHEST).is(Items.ELYTRA)) {
-            return;
-        }
-        if (!AccessoryEffectBridge.findFirstByEquipmentSlot(player, EquipmentSlot.CHEST).is(Items.ELYTRA)) {
-            return;
-        }
-        if (player.onGround() || player.isFallFlying() || player.isInWater() || player.hasEffect(MobEffects.LEVITATION) || !this.canGlide()) {
-            return;
-        }
-
-        this.startFallFlying();
-        cir.setReturnValue(true);
     }
 }
