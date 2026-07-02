d00e705b1dc3b762a394b04f4773d39c86b553a7
CasseShimada <c382304209@gmail.com>
2026-07-02 23:23:35 +0800

    Move player lifecycle callbacks into hooks

---
+++ src/main/java/com/aetherteam/aether/event/hooks/PlayerLifecycleHooks.java
@@ -0,0 +1,59 @@
+package com.aetherteam.aether.event.hooks;
+
+import com.aetherteam.aether.accessories.impl.AccessoryRuntime;
+import com.aetherteam.aether.attachment.AetherDataAttachments;
+import com.aetherteam.aether.network.PacketDistributor;
+import com.aetherteam.aether.network.packet.clientbound.RegisterMoaSkinsPacket;
+import com.aetherteam.aether.perk.types.MoaSkins;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+
+import javax.annotation.Nullable;
+
+public final class PlayerLifecycleHooks {
+    private PlayerLifecycleHooks() {
+    }
+
+    public static void login(ServerPlayer player) {
+        player.getAttachedOrCreate(AetherDataAttachments.AETHER_PLAYER).onLogin(player);
+        DimensionTimeHooks.syncAetherTime(player);
+        ServerPerkHooks.refreshPerks(player);
+        ToolAbilityHooks.setDebuffToolsState(player);
+        MoaSkins.registerMoaSkins(player.level());
+        PacketDistributor.sendToPlayer(player, new RegisterMoaSkinsPacket());
+        DimensionSpawnHooks.startInAether(player);
+        AccessoryRuntime.forceSync(player);
+    }
+
+    public static void logout(ServerPlayer player) {
+        player.getAttachedOrCreate(AetherDataAttachments.AETHER_PLAYER).onLogout(player);
+        AccessoryRuntime.clear(player);
+    }
+
+    public static void copyFrom(ServerPlayer oldPlayer, ServerPlayer newPlayer, boolean alive) {
+        newPlayer.getAttachedOrCreate(AetherDataAttachments.AETHER_PLAYER).handleRespawn(!alive);
+    }
+
+    public static void afterRespawn(ServerPlayer oldPlayer, ServerPlayer newPlayer, boolean alive) {
+        DimensionTimeHooks.syncAetherTime(newPlayer);
+        AccessoryRuntime.forceSync(newPlayer);
+    }
+
+    public static void changeLevel(ServerPlayer player, ServerLevel origin, ServerLevel destination) {
+        DimensionTravelHooks.remountPlayerAerbunny(player);
+        PlayerAttachmentSyncHooks.syncPlayerAttachment(player);
+        DimensionTimeHooks.syncAetherTime(player);
+        AccessoryRuntime.forceSync(player);
+    }
+
+    @Nullable
+    public static Player.BedSleepingProblem allowSleeping(Player player, BlockPos sleepingPos) {
+        return DimensionTimeHooks.isEternalDay(player) ? Player.BedSleepingProblem.OTHER_PROBLEM : null;
+    }
+
+    public static void joinLevel(Player player) {
+        player.getAttachedOrCreate(AetherDataAttachments.AETHER_PLAYER).onJoinLevel(player);
+    }
+}
--- src/main/java/com/aetherteam/aether/fabric/AetherFabricEvents.java
+++ src/main/java/com/aetherteam/aether/fabric/AetherFabricEvents.java
@@ -1,21 +1,13 @@
 package com.aetherteam.aether.fabric;
 
 import com.aetherteam.aether.accessories.impl.AccessoryRuntime;
-import com.aetherteam.aether.attachment.AetherDataAttachments;
 import com.aetherteam.aether.command.AetherCommands;
 import com.aetherteam.aether.event.hooks.BlockInteractionHooks;
-import com.aetherteam.aether.event.hooks.DimensionSpawnHooks;
 import com.aetherteam.aether.event.hooks.DimensionTimeHooks;
-import com.aetherteam.aether.event.hooks.DimensionTravelHooks;
 import com.aetherteam.aether.event.hooks.EntityEffectHooks;
 import com.aetherteam.aether.event.hooks.EntityGoalHooks;
 import com.aetherteam.aether.event.hooks.EntityInteractionHooks;
-import com.aetherteam.aether.event.hooks.PlayerAttachmentSyncHooks;
-import com.aetherteam.aether.event.hooks.ServerPerkHooks;
-import com.aetherteam.aether.event.hooks.ToolAbilityHooks;
-import com.aetherteam.aether.network.PacketDistributor;
-import com.aetherteam.aether.network.packet.clientbound.RegisterMoaSkinsPacket;
-import com.aetherteam.aether.perk.types.MoaSkins;
+import com.aetherteam.aether.event.hooks.PlayerLifecycleHooks;
 import net.fabricmc.fabric.api.entity.event.v1.EntitySleepEvents;
 import net.fabricmc.fabric.api.entity.event.v1.ServerEntityLevelChangeEvents;
 import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
@@ -43,41 +35,19 @@ public final class AetherFabricEvents {
     }
 
     private static void registerPlayerEvents() {
-        ServerPlayerEvents.JOIN.register(player -> {
-            player.getAttachedOrCreate(AetherDataAttachments.AETHER_PLAYER).onLogin(player);
-            DimensionTimeHooks.syncAetherTime(player);
-            ServerPerkHooks.refreshPerks(player);
-            ToolAbilityHooks.setDebuffToolsState(player);
-            MoaSkins.registerMoaSkins(player.level());
-            PacketDistributor.sendToPlayer(player, new RegisterMoaSkinsPacket());
-            DimensionSpawnHooks.startInAether(player);
-            AccessoryRuntime.forceSync(player);
-        });
-        ServerPlayerEvents.LEAVE.register(player -> {
-            player.getAttachedOrCreate(AetherDataAttachments.AETHER_PLAYER).onLogout(player);
-            AccessoryRuntime.clear(player);
-        });
-        ServerPlayerEvents.COPY_FROM.register((oldPlayer, newPlayer, alive) ->
-                newPlayer.getAttachedOrCreate(AetherDataAttachments.AETHER_PLAYER).handleRespawn(!alive));
-        ServerPlayerEvents.AFTER_RESPAWN.register((oldPlayer, newPlayer, alive) -> {
-            DimensionTimeHooks.syncAetherTime(newPlayer);
-            AccessoryRuntime.forceSync(newPlayer);
-        });
-        ServerEntityLevelChangeEvents.AFTER_PLAYER_CHANGE_LEVEL.register((player, origin, destination) -> {
-            DimensionTravelHooks.remountPlayerAerbunny(player);
-            PlayerAttachmentSyncHooks.syncPlayerAttachment(player);
-            DimensionTimeHooks.syncAetherTime(player);
-            AccessoryRuntime.forceSync(player);
-        });
-        EntitySleepEvents.ALLOW_SLEEPING.register((player, sleepingPos) ->
-                DimensionTimeHooks.isEternalDay(player) ? Player.BedSleepingProblem.OTHER_PROBLEM : null);
+        ServerPlayerEvents.JOIN.register(PlayerLifecycleHooks::login);
+        ServerPlayerEvents.LEAVE.register(PlayerLifecycleHooks::logout);
+        ServerPlayerEvents.COPY_FROM.register(PlayerLifecycleHooks::copyFrom);
+        ServerPlayerEvents.AFTER_RESPAWN.register(PlayerLifecycleHooks::afterRespawn);
+        ServerEntityLevelChangeEvents.AFTER_PLAYER_CHANGE_LEVEL.register(PlayerLifecycleHooks::changeLevel);
+        EntitySleepEvents.ALLOW_SLEEPING.register(PlayerLifecycleHooks::allowSleeping);
     }
 
     private static void registerEntityEvents() {
         ServerEntityEvents.ENTITY_LOAD.register((entity, world) -> {
             EntityGoalHooks.addGoals(entity);
             if (entity instanceof Player player) {
-                player.getAttachedOrCreate(AetherDataAttachments.AETHER_PLAYER).onJoinLevel(player);
+                PlayerLifecycleHooks.joinLevel(player);
             }
             if (entity instanceof net.minecraft.world.entity.LivingEntity livingEntity) {
                 AccessoryRuntime.forceSync(livingEntity);
