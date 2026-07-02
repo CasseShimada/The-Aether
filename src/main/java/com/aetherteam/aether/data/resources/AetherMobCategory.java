655a6fe2b4dd9f1771ee7d5fb6391baa2adce2dd
CasseShimada <c382304209@gmail.com>
2026-07-03 02:24:28 +0800

    Remove mob category enum extension fallback

--- src/main/java/com/aetherteam/aether/data/resources/AetherMobCategory.java
+++ src/main/java/com/aetherteam/aether/data/resources/AetherMobCategory.java
@@ -3,10 +3,10 @@ package com.aetherteam.aether.data.resources;
 import net.minecraft.world.entity.MobCategory;
 
 public class AetherMobCategory {
-    public static final MobCategory AETHER_SURFACE_MONSTER = resolve("AETHER_AETHER_SURFACE_MONSTER", MobCategory.MONSTER);
-    public static final MobCategory AETHER_DARKNESS_MONSTER = resolve("AETHER_AETHER_DARKNESS_MONSTER", MobCategory.MONSTER);
-    public static final MobCategory AETHER_SKY_MONSTER = resolve("AETHER_AETHER_SKY_MONSTER", MobCategory.MONSTER);
-    public static final MobCategory AETHER_AERWHALE = resolve("AETHER_AETHER_AERWHALE", MobCategory.CREATURE);
+    public static final MobCategory AETHER_SURFACE_MONSTER = MobCategory.MONSTER;
+    public static final MobCategory AETHER_DARKNESS_MONSTER = MobCategory.MONSTER;
+    public static final MobCategory AETHER_SKY_MONSTER = MobCategory.MONSTER;
+    public static final MobCategory AETHER_AERWHALE = MobCategory.CREATURE;
 
     public static boolean hasCustomSkyMonsterCategory() {
         return AETHER_SKY_MONSTER != MobCategory.MONSTER;
@@ -14,13 +14,5 @@ public class AetherMobCategory {
 
     public static boolean hasCustomAerwhaleCategory() {
         return AETHER_AERWHALE != MobCategory.CREATURE;
-    }
-
-    private static MobCategory resolve(String name, MobCategory fallback) {
-        try {
-            return MobCategory.valueOf(name);
-        } catch (IllegalArgumentException ignored) {
-            return fallback;
-        }
     }
 }
