fd5c3e7ffe03b36a89f7540b67722c9fe91fb7bc
CasseShimada <c382304209@gmail.com>
2026-07-15 12:03:20 +0800

    Migrate freezer to keyed registration

--- src/main/java/com/aetherteam/aether/block/AetherBlocks.java
+++ src/main/java/com/aetherteam/aether/block/AetherBlocks.java
@@ -94,7 +94,7 @@ public class AetherBlocks {
     public static final Block ENCHANTED_GRAVITITE = registerKeyed("enchanted_gravitite", key -> new FloatingBlock(true, Block.Properties.of().mapColor(MapColor.COLOR_PINK).instrument(NoteBlockInstrument.PLING).strength(5.0F, 6.0F).requiresCorrectToolForDrops().sound(SoundType.METAL).setId(key)));
 
     public static final Block ALTAR = registerKeyed("altar", key -> new AltarBlock(Block.Properties.of().mapColor(MapColor.WOOD).instrument(NoteBlockInstrument.BASEDRUM).strength(2.5F).setId(key)));
-    public static final Block FREEZER = register("freezer", () -> new FreezerBlock(Block.Properties.of().mapColor(MapColor.WOOD).instrument(NoteBlockInstrument.BASEDRUM).strength(2.0F)));
+    public static final Block FREEZER = registerKeyed("freezer", key -> new FreezerBlock(Block.Properties.of().mapColor(MapColor.WOOD).instrument(NoteBlockInstrument.BASEDRUM).strength(2.0F).setId(key)));
     public static final Block INCUBATOR = register("incubator", () -> new IncubatorBlock(Block.Properties.of().mapColor(MapColor.WOOD).instrument(NoteBlockInstrument.BASEDRUM).strength(2.0F)));
 
     public static final Block AMBROSIUM_WALL_TORCH = registerBlockOnly("ambrosium_wall_torch", () -> new WallTorchBlock(ParticleTypes.SMOKE, Block.Properties.ofFullCopy(Blocks.WALL_TORCH)));
